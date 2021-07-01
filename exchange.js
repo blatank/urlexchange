855910c3ea806e117fe0e530821af921da2dd39e
blatank <paddocknote@gmail.com>
2021-07-01 21:44:59 +0900

    first commit

---
+++ exchange.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+  let btn = document.getElementById('exchange');
+
+  // ボタンクリックしたときの処理
+  btn.onclick = function(){
+    // ①Editボックスのデータを取得して、日本語を変換する
+    alert("test");  // これはテスト用
+    // ②クリップボードにコピーする
+    
+  }
+})();
