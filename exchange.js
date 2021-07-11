c33bd8496e81e40018d902ef66c17e320f0cc0ef
blatank <paddocknote@gmail.com>
2021-07-11 19:58:59 +0900

    clipboardAPIで取りあえず実装

--- exchange.js
+++ exchange.js
@@ -1,12 +1,42 @@
 (function() {
   'use strict';
-  let btn = document.getElementById('exchange');
+  const btn = document.getElementById('exchange');
+  const edit = document.getElementById('urlEdit');
+  const chk = document.getElementById('check');
 
   // ボタンクリックしたときの処理
   btn.onclick = function(){
-    // ①Editボックスのデータを取得して、日本語を変換する
-    alert("test");  // これはテスト用
-    // ②クリップボードにコピーする
+    // ①Editボックスのデータを取得
+    const text = edit.value;
+
+    // ②URLエンコード
+    const encodedText = encodeURIComponent(text);
     
+    // ③クリップボードにコピーする
+    const ret = setClipboard(encodedText);
+
+    // ④チェックボックスにチェックがあればアラートを出す
+    if (chk.checked === true) {
+      if (ret === true) {
+        alert(`クリップボードに#{encodedText}をコピーしました！`);
+      }
+      else {
+        alert(`クリップボードにコピーするのに失敗しました!!`);
+      }
+    }
   }
+
+  // クリップボードにコピー
+  function setClipboard(text) {
+    let ret;
+    navigator.clipboard.writeText(text).then(function() {
+      /* clipboard successfully set */
+      ret = true;
+    }, function() {
+      /* clipboard write failed */
+      ret = false;
+    });
+    return ret;
+  }
+  
 })();
